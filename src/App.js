524ed9d7a27b67d2136ba922c8eeaf5827f9c6bb
kaynec <once462@gmail.com>
2021-04-26 01:25:36 +0430

    compressed the images a bit more

--- src/App.js
+++ src/App.js
@@ -4,28 +4,11 @@ import "./styles/reset.css";
 import Header from "./component/header";
 import Gameboard from "./component/gameboard";
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  useEffect(() => {
-    window.onload = (event) => {
-      setLoading(false);
-    };
-  }, []);
-  if (loading === true) {
-    return (
-      <div
-        style={{ margin: "30%" + "auto", width: "100%" }}
-        className="loading"
-      >
-        Loading The Website ,Please Wait ...
-      </div>
-    );
-  } else if (loading === false) {
-    return (
-      <>
-        <Header />
-        <Gameboard />
-      </>
-    );
-  }
+  return (
+    <>
+      <Header />
+      <Gameboard />
+    </>
+  );
 };
 export default App;
